6d563907bb1e80aff8aa5ff9e501f77aa3a2fee2
Gavin Morrice <gavin@gavinmorrice.com>
2022-04-01 17:20:22 +0100

    Add Rack::MiniProfiler with feature-toggle

--- spec/cookpad/performance/initialization_spec.rb
+++ spec/cookpad/performance/initialization_spec.rb
@@ -3,7 +3,9 @@ require "spec_helper"
 RSpec.describe "Cookpad::Performance::Initialization" do
   before do
     set_environment_variable("LOG_N_PLUS_ONE_QUERIES", nil)
+    set_environment_variable("PROFILE", nil)
     set_environment_variable("RAISE_N_PLUS_ONE_QUERIES", nil)
+    set_environment_variable("RACK_MINI_PROFILER", nil)
   end
 
   context "when LOG_N_PLUS_ONE_QUERIES is true" do
@@ -11,6 +13,7 @@ RSpec.describe "Cookpad::Performance::Initialization" do
       set_environment_variable("LOG_N_PLUS_ONE_QUERIES", "true")
 
       refresh_object_space!
+      load("config/initializers/n_plus_one_detection.rb")
 
       expect(Prosopite.instance_variable_get(:@rails_logger)).to be_truthy
     end
@@ -19,6 +22,7 @@ RSpec.describe "Cookpad::Performance::Initialization" do
       set_environment_variable("LOG_N_PLUS_ONE_QUERIES", "true")
 
       refresh_object_space!
+      load("config/initializers/n_plus_one_detection.rb")
 
       expect(Prosopite.instance_variable_get(:@prosopite_logger)).to be_truthy
     end
@@ -27,6 +31,7 @@ RSpec.describe "Cookpad::Performance::Initialization" do
       set_environment_variable("LOG_N_PLUS_ONE_QUERIES", "true")
 
       refresh_object_space!
+      load("config/initializers/n_plus_one_detection.rb")
 
       expect(Prosopite.instance_variable_get(:@stderr_logger)).to be_falsey
     end
@@ -36,6 +41,7 @@ RSpec.describe "Cookpad::Performance::Initialization" do
       set_environment_variable("RAISE_N_PLUS_ONE_QUERIES", "false")
 
       refresh_object_space!
+      load("config/initializers/n_plus_one_detection.rb")
 
       expect(Prosopite.instance_variable_get(:@raise)).to be_falsey
     end
@@ -46,6 +52,7 @@ RSpec.describe "Cookpad::Performance::Initialization" do
       set_environment_variable("LOG_N_PLUS_ONE_QUERIES", "false")
 
       refresh_object_space!
+      load("config/initializers/n_plus_one_detection.rb")
 
       expect(defined?(Prosopite)).to be_nil
     end
@@ -56,17 +63,73 @@ RSpec.describe "Cookpad::Performance::Initialization" do
       set_environment_variable("RAISE_N_PLUS_ONE_QUERIES", "true")
 
       refresh_object_space!
+      load("config/initializers/n_plus_one_detection.rb")
 
       expect(defined?(Prosopite)).to eql("constant")
     end
   end
 
+  context "when RACK_MINI_PROFILER and PROFILE are both true" do
+    it "raises an exception" do
+      set_environment_variable("RACK_MINI_PROFILER", "true")
+      set_environment_variable("PROFILE", "true")
+
+      refresh_object_space!
+
+      expect do
+        load("config/initializers/rack_mini_profiler.rb")
+      end.to raise_error(StandardError)
+    end
+  end
+
+  context "when RACK_MINI_PROFILER is true" do
+    it "loads rack mini profiler" do
+      set_environment_variable("RACK_MINI_PROFILER", "true")
+      build_test_app
+      refresh_object_space!
+
+      load("config/initializers/rack_mini_profiler.rb")
+
+      rmp = $LOADED_FEATURES.find do |file|
+        file.end_with?("rack-mini-profiler.rb")
+      end
+      expect(rmp).to be_present
+      expect(defined?(Rack::MiniProfiler)).to eql("constant")
+    end
+  end
+
+  context "when RACK_MINI_PROFILER is false" do
+    it "doesn't load rack mini profiler" do
+      set_environment_variable("RACK_MINI_PROFILER", "false")
+      build_test_app
+      refresh_object_space!
+
+      load("config/initializers/rack_mini_profiler.rb")
+
+      rmp = $LOADED_FEATURES.find do |file|
+        file.end_with?("rack-mini-profiler.rb")
+      end
+      expect(rmp).to be_nil
+      expect(defined?(Rack::MiniProfiler)).to be_nil
+    end
+  end
+
   private
 
-    def refresh_object_space!
+    def unload_prosopite
       $LOADED_FEATURES.delete_if { |filename| filename.end_with?("prosopite.rb") }
       Object.send(:remove_const, :Prosopite) if defined?(Prosopite)
+    end
+
+    def unload_rack_mini_profiler
+      $LOADED_FEATURES.delete_if { |filename| filename.end_with?("rack-mini-profiler.rb") }
+      Rack.send(:remove_const, :MiniProfiler) if defined?(Rack::MiniProfiler)
+      Rack.send(:remove_const, :MiniProfilerRails) if defined?(Rack::MiniProfilerRails)
+    end
+
+    def refresh_object_space!
+      unload_prosopite
+      unload_rack_mini_profiler
       load("cookpad/performance.rb")
-      load("config/initializers/n_plus_one_detection.rb")
     end
 end
