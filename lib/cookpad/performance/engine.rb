d76d7dc9ef62bebb4233ee6c072046f9bdd5524c
Gavin Morrice <gavin@gavinmorrice.com>
2022-05-10 16:54:00 +0100

    Add controller concerns to load path

--- lib/cookpad/performance/engine.rb
+++ lib/cookpad/performance/engine.rb
@@ -3,6 +3,8 @@ module Cookpad
     class Engine < ::Rails::Engine
       isolate_namespace Cookpad::Performance
 
+      paths.add "app/controllers/concerns", eager_load: true, glob: "app/controllers/concerns"
+
       config.to_prepare do
         if !Rails.env.production? &&
            (ENV["LOG_N_PLUS_ONE_QUERIES"] == "true" ||
