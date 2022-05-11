e10f49381c8c44b5e9efdcb3f20ee03d913975f1
Gavin Morrice <gavin@gavinmorrice.com>
2022-05-11 09:15:56 +0100

    Update lib/cookpad/performance/engine.rb
    Co-authored-by: imashin - Shinsuke IMAI <imaishinsuke@gmail.com>

--- lib/cookpad/performance/engine.rb
+++ lib/cookpad/performance/engine.rb
@@ -5,7 +5,7 @@ module Cookpad
 
       config.to_prepare do
         if !Rails.env.production? &&
-           (ENV["LOG_N_PLUS_ONE_QUERIES"] == "true" ||
+          (ENV["LOG_N_PLUS_ONE_QUERIES"] == "true" ||
              ENV["RAISE_N_PLUS_ONE_QUERIES"] == "true")
           require "cookpad/performance/n_plus_one_detection"
 
