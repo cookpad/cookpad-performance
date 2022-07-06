0e588f379dc4d3cb04add908148918f453a30305
Gavin Morrice <gavin@gavinmorrice.com>
2022-07-06 16:18:01 +0100

    Fix Rubocop violations

--- config/initializers/n_plus_one_detection.rb
+++ config/initializers/n_plus_one_detection.rb
@@ -1,5 +1,5 @@
 if (ENV["LOG_N_PLUS_ONE_QUERIES"] == "true" || ENV["RAISE_N_PLUS_ONE_QUERIES"] == "true") &&
-  (Rails.env.development? || Rails.env.test?)
+   (Rails.env.development? || Rails.env.test?)
   require "prosopite"
   Prosopite.rails_logger = ENV["LOG_N_PLUS_ONE_QUERIES"] == "true"
   Prosopite.prosopite_logger = true
--- lib/cookpad/performance/railtie.rb
+++ lib/cookpad/performance/railtie.rb
@@ -1,9 +1,10 @@
 module Cookpad
   module Performance
     class Railtie < ::Rails::Railtie
-      DEFAULTS_IVAR_NAME = "@defaults"
+      DEFAULTS_IVAR_NAME = "@defaults".freeze
+      private_constant :DEFAULTS_IVAR_NAME
 
-      config.after_initialize  do
+      config.after_initialize do
         if ENV["PROFILE"] && Rails.env.development? && defined?(Webpacker)
           default_config = Webpacker.config.instance_variable_get(DEFAULTS_IVAR_NAME)
           default_config["compile"] = false
