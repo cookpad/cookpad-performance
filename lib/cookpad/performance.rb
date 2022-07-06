dddd8ced48ef43ecb9d84bfde0d3f64165cd2816
Gavin Morrice <gavin@gavinmorrice.com>
2022-07-06 15:44:05 +0100

    Add empty Railtie file

--- lib/cookpad/performance.rb
+++ lib/cookpad/performance.rb
@@ -1,6 +1,7 @@
 require "rails"
 require "cookpad/performance/version"
 require "cookpad/performance/engine"
+require "cookpad/performance/railtie" if defined?(Rails::Railtie)
 
 module Cookpad
   module Performance
---
+++ lib/cookpad/performance/railtie.rb
@@ -0,0 +1,6 @@
+module Cookpad
+  module Performance
+    class Railtie < ::Rails::Railtie
+    end
+  end
+end
