64d2e2810128088b36d0b5ecb891a8307fcf078d
Gavin Morrice <gavin@gavinmorrice.com>
2022-07-06 16:04:12 +0100

    Disable webpacker in development if PROFILE mode

--- lib/cookpad/performance/railtie.rb
+++ lib/cookpad/performance/railtie.rb
@@ -1,6 +1,15 @@
 module Cookpad
   module Performance
     class Railtie < ::Rails::Railtie
+      DEFAULTS_IVAR_NAME = "@defaults"
+
+      config.after_initialize  do
+        if ENV["PROFILE"] && Rails.env.development? && defined?(Webpacker)
+          default_config = Webpacker.config.instance_variable_get(DEFAULTS_IVAR_NAME)
+          default_config["compile"] = false
+          Webpacker.config.instance_variable_set(DEFAULTS_IVAR_NAME, default_config)
+        end
+      end
     end
   end
 end
